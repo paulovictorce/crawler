debaabff50c53752dbd5f1bc31c50527d91f17d6
paulohome <bd.paulovictor@gmail.com>
2018-01-25 20:34:05 -0300

    Scrapy

--- crawler/spiders/pokemon.py
+++ crawler/spiders/pokemon.py
@@ -1,9 +1,16 @@
 import scrapy
 
-class PokemonSpider(scrapy.spider)
+
+class PokemonSpider(scrapy.Spider):
     name = 'pokemon'
 
     def start_request(self):
-
+        yield scrapy.Request(url='https://pokemondb.net/pokedex/national', callback=self.parse)
 
     def parse(self, response):
+        identificador = response.css('span.infocard-tall small::text').extract_first()
+        nome = response.css('span.infocard-tall .ent-name::text').extract_first()
+        tipo = response.css('span.infocard-tall small.aside::text').extract_first()
+        self.log('Name : %s' % nome)
+        self.log('Id : %s' % identificador)
+        self.log('Tipo : %s' % tipo)
