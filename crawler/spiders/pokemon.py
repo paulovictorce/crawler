51a8a9bb61b56e0270b44aee77474b47acb2cdd3
paulohome <bd.paulovictor@gmail.com>
2018-02-03 07:29:44 -0300

    Correção método requests - Crawler

--- crawler/spiders/pokemon.py
+++ crawler/spiders/pokemon.py
@@ -4,10 +4,11 @@ import scrapy
 class PokemonSpider(scrapy.Spider):
     name = 'pokemon'
 
-    def start_request(self):
-        yield scrapy.Request(url='https://pokemondb.net/pokedex/national', callback=self.parse)
+    def start_requests(self):
+        yield scrapy.Request(url = 'https://pokemondb.net/pokedex/national', callback = self.parse)
 
     def parse(self, response):
+        self.log('Visited : %s' % response.url)
         identificador = response.css('span.infocard-tall small::text').extract_first()
         nome = response.css('span.infocard-tall .ent-name::text').extract_first()
         tipo = response.css('span.infocard-tall small.aside::text').extract_first()
